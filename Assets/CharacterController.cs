7fde1cfc0f343c05ace005409289a663d09907f9
I-Bearman <i-medvedev92@yandex.ru>
2022-06-04 13:34:24 +0700

    Changes 04.06

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,24 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private Vector3 target;
-    // Start is called before the first frame update
+    [SerializeField] private Transform spawn_point;
+    [SerializeField] private GameObject targetPref;
+    private GameObject target;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = target;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (target == null)
         {
+            target = TargetSpawn();
+            agent.destination = target.transform.position;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Alpha1)) ||  Vector3.Distance(transform.position, target.transform.position) < 1)   //!!!!!!!!!
+        {
+            Destroy(target);
         }
     }
+
+
+        private GameObject TargetSpawn()
+    {
+        float volumeX = spawn_point.transform.localScale.x / 2;
+        float volumeZ = spawn_point.transform.localScale.z / 2;
+
+        Vector3 pos = new Vector3(Random.Range(spawn_point.position.x - volumeX, spawn_point.position.x + volumeX), spawn_point.position.y + 1, Random.Range(spawn_point.position.z - volumeZ, spawn_point.position.z + volumeZ));
+        GameObject obj = Instantiate(targetPref, pos, Quaternion.identity);
+        return obj;
+    }
+
+
 }
